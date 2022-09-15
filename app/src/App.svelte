3d105eb453739fe0860bf97c67be2512630da4c6
Justin Stitt <cpt.crixus@gmail.com>
2022-09-15 01:00:01 -0700

    logo setup

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,19 @@
 <script>
-	export let name;
+  import Memory from "./Memory.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="main">
+  <h1>ACM Memory</h1>
+  <Memory />
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+  }
 </style>
---
+++ app/src/Memory.svelte
@@ -0,0 +1,76 @@
+<script>
+  import { poppress } from "./poppress.js";
+  const logosize = 225;
+  const popconfig = { newWidth: logosize * 1.5, defaultTimeout: 75 };
+  const click = () => {
+    console.log("clicked");
+  };
+</script>
+
+<div class="main">
+  <img
+    class="logo"
+    id="designl"
+    src="design-logo.svg"
+    alt="ACM Design Team Logo"
+    width={logosize}
+    use:poppress={popconfig}
+  />
+  <img
+    class="logo"
+    id="devl"
+    src="dev-logo.svg"
+    alt="ACM Dev Team Logo"
+    width={logosize}
+    use:poppress={popconfig}
+  />
+  <img
+    class="logo"
+    id="algol"
+    src="algo-logo.svg"
+    alt="ACM Algo Team Logo"
+    width={logosize}
+    use:poppress={popconfig}
+  />
+  <img
+    class="logo"
+    id="ail"
+    src="ai-logo.svg"
+    alt="ACM AI Team Logo"
+    width={logosize}
+    use:poppress={popconfig}
+  />
+</div>
+
+<style>
+  .main {
+    background-color: rebeccapurple;
+  }
+
+  .logo {
+    position: fixed;
+    transform: translateX(-50%) rotateZ(45deg);
+    transition: 0.4s ease-out;
+    background-color: rgba(52, 160, 92, 0.472);
+  }
+
+  #ail {
+    left: 50%;
+    top: 55%;
+  }
+
+  #algol {
+    left: 40%;
+    top: 35%;
+  }
+
+  #devl {
+    left: 60%;
+    top: 35%;
+  }
+
+  #designl {
+    left: 50%;
+    top: 15%;
+  }
+</style>
